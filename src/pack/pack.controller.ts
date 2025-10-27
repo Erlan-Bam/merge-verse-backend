87e3d03a96b1dd53bfb843680e8e95076a6d5abb
Erlan-Bam <erlanzh.gg@gmail.com>
2025-10-27 21:33:55 +0500

    feat: updated

--- src/pack/pack.controller.ts
+++ src/pack/pack.controller.ts
@@ -1,8 +1,14 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, UseGuards } from '@nestjs/common';
 import { PackService } from './pack.service';
 import { User } from 'src/shared/decorator/user.decorator';
+import { Api } from 'grammy';
+import { ApiBearerAuth, ApiTags } from '@nestjs/swagger';
+import { AuthGuard } from '@nestjs/passport';
 
 @Controller('pack')
+@ApiTags('Pack')
+@ApiBearerAuth('JWT')
+@UseGuards(AuthGuard('jwt'))
 export class PackController {
   constructor(private packService: PackService) {}
 
