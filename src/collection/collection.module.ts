3d6045890b522a41f5b7a9b60b09fbedaef2c9dd
Erlan-Bam <erlanzh.gg@gmail.com>
2025-10-28 00:35:07 +0500

    feat: updated

--- src/collection/collection.module.ts
+++ src/collection/collection.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { CollectionController } from './collection.controller';
 import { CollectionService } from './collection.service';
+import { GiftModule } from 'src/gift/gift.module';
 
 @Module({
+  imports: [GiftModule],
   controllers: [CollectionController],
-  providers: [CollectionService]
+  providers: [CollectionService],
 })
 export class CollectionModule {}
