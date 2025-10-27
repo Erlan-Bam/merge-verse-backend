707a58404f9cc24a19c407ce806cb2a5f621170b
Erlan-Bam <erlanzh.gg@gmail.com>
2025-10-27 21:24:24 +0500

    feat: updated

--- src/shared/shared.module.ts
+++ src/shared/shared.module.ts
@@ -4,10 +4,19 @@ import { CronService } from './services/cron.service';
 import { JwtStrategy } from './strategies/jwt.strategy';
 import { JwtModule, JwtService } from '@nestjs/jwt';
 import { BotService } from './services/bot.service';
+import { ConfigService } from '@nestjs/config';
 
 @Global()
 @Module({
-  imports: [JwtModule.register({})],
+  imports: [
+    JwtModule.registerAsync({
+      useFactory: (configService: ConfigService) => ({
+        secret: configService.getOrThrow<string>('JWT_SECRET'),
+        signOptions: { expiresIn: '30d' },
+      }),
+      inject: [ConfigService],
+    }),
+  ],
   providers: [PrismaService, BotService, CronService, JwtStrategy, JwtService],
   exports: [PrismaService, BotService, CronService, JwtStrategy, JwtService],
 })
